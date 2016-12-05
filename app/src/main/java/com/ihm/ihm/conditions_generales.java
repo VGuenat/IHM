47442dd181fb41a48ca2c46eed288202ce5c85e2
Vincent Guenat <guenatv@gmail.com>
2016-12-05 10:54:27 +0100

    Création de toutes les pages, retrait du floating button et du OptionMenu

--- app/src/main/java/com/ihm/ihm/conditions_generales.java
+++ app/src/main/java/com/ihm/ihm/conditions_generales.java
@@ -2,10 +2,7 @@ package com.ihm.ihm;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.text.method.ScrollingMovementMethod;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -50,28 +47,6 @@ public class Conditions_generales extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.conditions_generales, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -82,15 +57,20 @@ public class Conditions_generales extends AppCompatActivity
             Intent i = new Intent(this, Accueil.class);
             startActivity(i);
         } else if (id == R.id.nav_profil) {
-
+            Intent i = new Intent(this, Mon_profil.class);
+            startActivity(i);
         } else if (id == R.id.nav_panier) {
-
+            Intent i = new Intent(this, Mon_panier.class);
+            startActivity(i);
         } else if (id == R.id.nav_historique) {
-
+            Intent i = new Intent(this, Mon_historique.class);
+            startActivity(i);
         } else if (id == R.id.nav_notif) {
-
+            Intent i = new Intent(this, Mes_notifications.class);
+            startActivity(i);
         } else if (id == R.id.nav_politique) {
-
+            Intent i = new Intent(this, Politique_securite.class);
+            startActivity(i);
         } else if (id == R.id.nav_condition) {
             Intent i = new Intent(this, Conditions_generales.class);
             startActivity(i);
